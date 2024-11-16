8e824a4349cebcf78ed30d95741457c98b2e220b
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-16 06:37:28 +0100

    feat: read json from test file

--- synth-converter/src/main.rs
+++ synth-converter/src/main.rs
@@ -1,15 +1,13 @@
-// "Import" anything public in the parser module
+use std::fs;
+
 pub mod parser;
 
 fn main() {
     println!("Hello, world!");
 
-    // Parse the JSON
-    let result = parser::untyped_example();
+    // Grab JSON file
+    let file_path = "data/test.json".to_owned();
+    let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
 
-    // Handle errors from the parser if any
-    match result {
-        Ok(_result) => (),
-        Err(error) => print!("{}", error),
-    }
+    let _ = parser::untyped_example(&contents);
 }
--- synth-converter/src/parser.rs
+++ synth-converter/src/parser.rs
@@ -1,19 +1,7 @@
 use serde_json::{Result, Value};
 
-pub fn untyped_example() -> Result<()> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = r#"
-        {
-            "name": "John Doe",
-            "age": 43,
-            "phones": [
-                "+44 1234567",
-                "+44 2345678"
-            ]
-        }"#;
-
-    // Parse the string of data into serde_json::Value.
-    let v: Value = serde_json::from_str(data)?;
+pub fn untyped_example(json_data: &str) -> Result<()> {
+	let v: Value = serde_json::from_str(json_data)?;
 
     // Access parts of the data by indexing with square brackets.
     println!("Please call {} at the number {}", v["name"], v["phones"][0]);
