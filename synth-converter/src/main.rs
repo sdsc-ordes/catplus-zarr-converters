91b6444b3d745cc80b49ac2126bdf8cdf307c275
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-16 06:37:28 +0100

    feat: add json parser and simple example

--- synth-converter/src/main.rs
+++ synth-converter/src/main.rs
@@ -1,3 +1,15 @@
+// "Import" anything public in the parser module
+pub mod parser;
+
 fn main() {
     println!("Hello, world!");
+
+    // Parse the JSON
+    let result = parser::untyped_example();
+
+    // Handle errors from the parser if any
+    match result {
+        Ok(_result) => (),
+        Err(error) => print!("{}", error),
+    }
 }
