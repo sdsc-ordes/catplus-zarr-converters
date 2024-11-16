834b4b8f75f7e6a1cb5045e56fa73bb0d51b0295
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-16 14:40:44 +0100

    feat: add sophia and build a graph

--- synth-converter/src/main.rs
+++ synth-converter/src/main.rs
@@ -1,14 +1,43 @@
 use std::fs;
 mod parser;
-
+use sophia::api::prelude::*;
+use sophia::api::ns::Namespace;
+use sophia::inmem::graph::LightGraph;
+use sophia::turtle::parser::turtle;
+use sophia::turtle::serializer::nt::NtSerializer;
 use parser::parser::typed_example;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 
     // Grab JSON file
     let file_path = "data/test.json".to_owned();
     let contents = fs::read_to_string(file_path).expect("Couldn't find or load that file.");
 
-    let _ = typed_example(&contents);
+    let _ = typed_example(&contents);    
+    // Loading a graph
+    let example = r#"
+        @prefix : <http://example.org/>.
+        @prefix foaf: <http://xmlns.com/foaf/0.1/>.
+        :alice foaf:name "Alice";
+               foaf:mbox <mailto:alice@work.example> .
+        :bob foaf:name "Bob".
+    "#;
+    let mut graph: LightGraph = turtle::parse_str(example).collect_triples()?;
+    
+    // Mutating the graph
+    let ex = Namespace::new("http://example.org/")?;
+    let foaf = Namespace::new("http://xmlns.com/foaf/0.1/")?;
+    graph.insert(
+        ex.get("bob")?,
+        foaf.get("knows")?,
+        ex.get("alice")?,
+    )?;
+    
+    // Serializing the graph
+    let mut nt_stringifier = NtSerializer::new_stringifier();
+    let example2 = nt_stringifier.serialize_graph(&graph)?.as_str().to_string();
+    println!("The resulting graph:\n{}", example2);
+
+    Ok(())
 }
