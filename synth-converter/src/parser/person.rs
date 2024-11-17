dfaa8a5b861125c27a95c03e0422cefb7445f3e4
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-17 13:08:04 +0100

    chore: remove unneeded person struct

--- synth-converter/src/parser/person.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use std::clone::Clone;
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Person {
-    pub name: String,
-    pub age: u8,
-    pub phones: Vec<String>,
-}
