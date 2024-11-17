9778d43a4b4f25908c7aa3f510fea27868e929fa
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-17 13:08:26 +0100

    chore: remove log statements in parser

--- synth-converter/src/parser/parser.rs
+++ synth-converter/src/parser/parser.rs
@@ -4,9 +4,6 @@ pub fn parse_json(json_data: &str) -> serde_json::Result<Batch> {
     // Parse the JSON string into a `Batch` object
     let batch: Batch = serde_json::from_str(json_data)?;
 
-    // Log the parsed person object for debugging
-    //println!("{:?}", batch);
-
     // Return the `batch` object
     Ok(batch)
 }
