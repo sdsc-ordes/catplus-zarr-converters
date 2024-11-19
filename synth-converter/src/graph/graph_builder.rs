031fe08b2f895ed852ed132291484b142274c4c7
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-19 15:55:03 +0100

    feat: clean up graph builder

--- synth-converter/src/graph/graph_builder.rs
+++ synth-converter/src/graph/graph_builder.rs
@@ -1,11 +1,12 @@
-use std::collections::HashMap;
 use sophia::api::graph::MutableGraph;
 use sophia::api::ns::Namespace;
 use sophia::api::serializer::TripleSerializer;
 use sophia::inmem::graph::LightGraph;
-use sophia_api::term::bnode_id::BnodeId;
 use sophia_turtle::serializer::turtle::TurtleSerializer;
 use crate::parser::batch::Batch;
+use crate::parser::batch::Action;
+use sophia_api::ns::NsTerm;
+use crate::graph::utils::generate_unique_identifier;
 use lazy_static::lazy_static;
 
 lazy_static! {
@@ -17,98 +18,58 @@ lazy_static! {
 }
 
 pub struct GraphBuilder {
-    action_counter: HashMap<String, usize>,
     graph: LightGraph,
 }
 
 impl GraphBuilder {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
         Ok(Self {
-            action_counter: HashMap::new(),
             graph: LightGraph::new(),
         })
     }
 
-    fn get_action_uri(&mut self, action_name: &str)  -> String {
-        // Increment the counter for the given action name
-        let count = self.action_counter.entry(action_name.to_string()).or_insert(0);
-        *count += 1;
+    fn add_action_to_graph(
+        &mut self,
+        batch_uri: &NsTerm<'_>,
+        action: &Action,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        match action {
+            Action::filtrateAction(filtrate_action) => {
+                let action_id = generate_unique_identifier();
+                let action_uri = EX.get(&action_id)?;
 
-        // Generate a unique URI
-        format!("{}_{}", action_name, *count)
-    }
-
-    fn get_action_iri_name(&self, action_name: &str) -> String {
-        let mapped_class = match action_name {
-            "add" => "AddAction",
-            "set_temperature" => "setTemperatureAction",
-            _ => "AFRE_0000001",
-        };
-        mapped_class.to_string()
+                self.graph.insert(&action_uri, &CAT.get("hasBatch")?, batch_uri)?;
+                self.graph.insert(&action_uri, &RDF.get("type")?, &CAT.get("FiltrateAction")?)?;
+                self.graph.insert(&action_uri, &ALLORES.get("AFX_0000622")?, filtrate_action.startTime.as_str())?;
+                self.graph.insert(&action_uri, &ALLORES.get("AFR_0001606")?, filtrate_action.method_name.as_str())?;
+                self.graph.insert(&action_uri, &ALLORES.get("AFR_0001723")?, filtrate_action.equipment_name.as_str())?;
+                self.graph.insert(&action_uri, &CAT.get("localEquipmentName")?, filtrate_action.sub_equipment_name.as_str())?;
+                self.graph.insert(&action_uri, &CAT.get("containerID")?, filtrate_action.containerID.as_str())?;
+                self.graph.insert(&action_uri, &CAT.get("containerBarcode")?, filtrate_action.containerBarcode.as_str())?;
+            }
+        }
+        Ok(())
     }
 
     pub fn add_batch(&mut self, batch: &Batch) -> Result<(), Box<dyn std::error::Error>> {
-        let batch_uri = EX.get(&batch.batch_id)?.clone();
+        let batch_id = generate_unique_identifier();
+        let batch_uri = EX.get(&batch_id)?;
         let rdf_type = RDF.get("type")?;
-
-        self.graph.insert(
-            &batch_uri,
-            &ALLORES.get("AFR_0001120")?,
-            batch.batch_id.as_str(),
-        )?;
 
         self.graph.insert(
             &batch_uri,
             &rdf_type,
             &CAT.get("Batch")?,
         )?;
+        self.graph.insert(
+            &batch_uri,
+            &SCHEMA.get("name")?,
+            batch.batchID.as_str(),
+        )?;
 
         for action in &batch.actions {
-            let unique_action_name = self.get_action_uri(&action.name).clone();
-
-            let action_bnode = BnodeId::new_unchecked(&*unique_action_name);
-            self.graph.insert(
-                &batch_uri,
-                &ALLORES.get("AFRE_0000001")?,
-                &action_bnode,
-            )?;
-
-            let action_iri_name = self.get_action_iri_name(&action.name);
-            let mapped_action = CAT.get(&action_iri_name)?.clone();
-            self.graph
-                .insert(
-                    &action_bnode,
-                    &rdf_type,
-                    &mapped_action,
-                )
-                .unwrap();
-                let schema_name = SCHEMA.get("name")?;
-                let cat_local_name = CAT.get("localEquipmentName")?;
-                let cat_barcode = CAT.get("containerBarcode")?;
-                let cat_dispense_type = CAT.get("dispenseType")?;
-                let action_predicates = vec![
-                    (Some(action.name.as_str()), &schema_name),
-                    (
-                        action.equipment_local_name.as_ref().map(|x| x.as_str()),
-                        &cat_local_name,
-                    ),
-                    (
-                        action.container_barcode.as_ref().map(|x| x.as_str()),
-                        &cat_barcode,
-                    ),
-                    (
-                        action.dispense_type.as_ref().map(|x| x.as_str()),
-                        &cat_dispense_type,
-                    ),
-                ];
-                for (field, predicate) in action_predicates {
-                    if let Some(value) = field {
-                        // Use `&*predicate` to convert `&NsTerm<'_>` to `&&NsTerm<'_>`
-                        self.graph.insert(&action_bnode, &*predicate, value)?;
-                    }
-                }
+            self.add_action_to_graph(&batch_uri, action)?;
         }
-
         Ok(())
     }
 
