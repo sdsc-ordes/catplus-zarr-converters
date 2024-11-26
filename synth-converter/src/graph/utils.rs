fb8a8770fc339287b1cc46cc4d63ba89c894ad3b
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-26 21:58:26 +0100

    feat: add utilities for blank nodes and uris

---
+++ synth-converter/src/graph/utils.rs
@@ -0,0 +1,20 @@
+use crate::graph::namespaces::EX;
+use uuid::Uuid;
+use sophia_api::term::{bnode_id::BnodeId, SimpleTerm};
+use sophia_api::prelude::*;
+
+pub fn generate_bnode_term() -> SimpleTerm<'static> {
+    let identifier = Uuid::new_v4().to_string();
+    let bnode = BnodeId::new_unchecked(identifier);
+    bnode
+        .try_into_term()
+        .expect("Failed to convert BnodeId to SimpleTerm")
+}
+
+pub fn generate_uri_term() -> Result<SimpleTerm<'static>, Box<dyn std::error::Error>> {
+    let identifier = Uuid::new_v4().to_string();
+    let node_uri = EX.get(&identifier)?;
+    node_uri
+        .try_into_term()
+        .map_err(|e| Box::new(e) as Box<dyn std::error::Error>) // Convert error type
+}
