88acd37e40cc132af4bffe2c232026a53b7bce97
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-27 17:04:35 +0100

    chore: document parser function

--- synth-converter/src/parser/parser.rs
+++ synth-converter/src/parser/parser.rs
@@ -1,6 +1,16 @@
 use crate::parser::actions::Batch;
 
 #[allow(dead_code)]
+/// Parses a JSON string into a `Batch` struct.
+///
+/// The input JSON should conform to the structure defined in the `Batch` struct.
+/// An example input file, `1-Synth.json`, is available in the `example` directory.
+///
+/// # Arguments
+/// * `json_data` - A JSON string to be parsed.
+///
+/// # Returns
+/// A `Result` containing the parsed `Batch` struct or a `serde_json::Error` if pa
 pub fn parse_json(json_data: &str) -> serde_json::Result<Batch> {
     let batch: Batch = serde_json::from_str(json_data)?;
 
