01581db4d44e7b4f5f8d53187465da9552cc32bd
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-27 07:05:49 +0100

    fix: remove unused code warning that is due to lib.rs

--- synth-converter/src/parser/parser.rs
+++ synth-converter/src/parser/parser.rs
@@ -1,5 +1,6 @@
 use crate::parser::actions::Batch;
 
+#[allow(dead_code)]
 pub fn parse_json(json_data: &str) -> serde_json::Result<Batch> {
     let batch: Batch = serde_json::from_str(json_data)?;
     Ok(batch)
