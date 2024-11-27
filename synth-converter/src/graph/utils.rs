9ae1b0a5f05a5a3c88b4938df06c247ee83c3a5b
Sabine Maennel <sabinem@users.noreply.github.com>
2024-11-27 09:38:05 +0100

    style: add blank line before function output
    Co-authored-by: Gabriel Nützi <gnuetzi@gmail.com>

--- synth-converter/src/graph/utils.rs
+++ synth-converter/src/graph/utils.rs
@@ -8,6 +8,7 @@ use uuid::Uuid;
 pub fn generate_bnode_term() -> SimpleTerm<'static> {
     let identifier = Uuid::new_v4().to_string();
     let bnode = BnodeId::new_unchecked(identifier);
+
     bnode
         .try_into_term()
         .expect("Failed to convert BnodeId to SimpleTerm")
