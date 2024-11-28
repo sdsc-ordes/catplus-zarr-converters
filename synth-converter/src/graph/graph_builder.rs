de464697d7ecac6a2a39f8e38c4a0a1f971806d8
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-28 11:30:59 +0100

    refactor: make turtle serialization reusable also for other graphs

--- synth-converter/src/graph/graph_builder.rs
+++ synth-converter/src/graph/graph_builder.rs
@@ -19,6 +19,29 @@ use sophia::{
 use sophia_api::{ns::NsTerm, term::SimpleTerm};
 use sophia_turtle::serializer::turtle::{TurtleConfig, TurtleSerializer};
 
+/// Serialize an RDF graph to Turtle format
+///
+/// # Parameters
+/// - `graph`: A reference to the graph to be serialized.
+///
+/// # Returns
+/// A `Result` containing the Turtle serialization as a `String`, or an error if serialization fails.
+pub fn serialize_graph_to_turtle(
+    graph: &LightGraph,
+) -> Result<String, Box<dyn std::error::Error>> {
+
+    let prefix_map = generate_prefix_map();
+
+    let config = TurtleConfig::default()
+        .with_pretty(true)
+        .with_own_prefix_map(prefix_map);
+
+    let mut serializer = TurtleSerializer::new_stringifier_with_config(config);
+    serializer.serialize_graph(graph)?;
+
+    Ok(serializer.as_str().to_string())
+}
+
 /// An RDF Graph
 pub struct GraphBuilder {
     graph: LightGraph,
@@ -404,20 +427,14 @@ impl GraphBuilder {
         Ok(())
     }
 
-    /// Get the turtle serilaization of the RDF graph
+    /// Get the turtle serialization of the RDF graph
     ///
     /// Assumes a new graph has been created and built.
     ///
     /// # Returns
-    /// A `Result` containing the graph as turtle serialization, or an error
+    /// A `Result` containing the graph as Turtle serialization, or an error
     /// if the graph retrieval fails.
     pub fn serialize_to_turtle(&self) -> Result<String, Box<dyn std::error::Error>> {
-        let prefix_map = generate_prefix_map();
-        let config = TurtleConfig::default()
-            .with_pretty(true)
-            .with_own_prefix_map(prefix_map);
-        let mut serializer = TurtleSerializer::new_stringifier_with_config(config);
-        serializer.serialize_graph(&self.graph)?;
-        Ok(serializer.as_str().to_string())
+        serialize_graph_to_turtle(&self.graph)
     }
 }
