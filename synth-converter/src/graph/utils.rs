e35aef89c3a38f6a0864fa9f3f06f58fa86ebf1e
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-29 20:06:02 +0100

    chore: remove unused uri_generator from utils

--- synth-converter/src/graph/utils.rs
+++ synth-converter/src/graph/utils.rs
@@ -1,4 +1,3 @@
-use crate::graph::namespaces::EX;
 use sophia_api::{
     prelude::*,
     term::{bnode_id::BnodeId, SimpleTerm},
@@ -12,13 +11,4 @@ pub fn generate_bnode_term() -> SimpleTerm<'static> {
     bnode
         .try_into_term()
         .expect("Failed to convert BnodeId to SimpleTerm")
-}
-
-pub fn generate_uri_term() -> Result<SimpleTerm<'static>, Box<dyn std::error::Error>> {
-    let identifier = Uuid::new_v4().to_string();
-    let node_uri = EX.get(&identifier)?;
-
-    node_uri
-        .try_into_term()
-        .map_err(|e| Box::new(e) as Box<dyn std::error::Error>) // Convert error type
 }
