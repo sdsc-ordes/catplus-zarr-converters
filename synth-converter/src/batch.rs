53965e708a7b51f2de94cba5f3fd0d5725e01394
sabinem <5292683+sabinem@users.noreply.github.com>
2025-01-20 16:54:13 +0100

    feat: improve batch struct
    Add `#[serde(rename_all = "camelCase")]` to set a default transformation and only add individual field names where the naming deviates from that pattern

--- synth-converter/src/batch.rs
+++ synth-converter/src/batch.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Batch {
     #[serde(rename = "batchID")]
     pub batch_id: String,
     #[serde(rename = "Actions")]
     pub actions: Vec<Action>,
-    #[serde(rename = "batchName")]
     pub batch_name: Option<String>,
     #[serde(rename = "ReactionType")]
     pub reaction_type: Option<String>,
@@ -17,38 +17,24 @@ pub struct Batch {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Action {
-    #[serde(rename = "actionName")]
     pub action_name: ActionName,
-    #[serde(rename = "startTime")]
     pub start_time: String,
-    #[serde(rename = "endingTime")]
     pub ending_time: String,
-    #[serde(rename = "methodName")]
     pub method_name: String,
-    #[serde(rename = "equipmentName")]
     pub equipment_name: String,
-    #[serde(rename = "subEquipmentName")]
     pub sub_equipment_name: String,
     #[serde(flatten)]
     pub container_info: Option<ContainerInfo>,
-    #[serde(rename = "speedShaker")]
     pub speed_shaker: Option<Observation>,
-    #[serde(rename = "hasContainerPositionAndQuantity")]
     pub has_container_position_and_quantity: Option<Vec<ContainerPositionQuantityItem>>,
-    #[serde(rename = "dispenseState")]
     pub dispense_state: Option<String>,
-    #[serde(rename = "dispenseType")]
     pub dispense_type: Option<String>,
-    #[serde(rename = "hasSample")]
     pub has_sample: Option<Sample>,
-    #[serde(rename = "speedTumbleStirrer")]
     pub speed_tumble_stirrer: Option<Observation>,
-    #[serde(rename = "temperatureTumbleStirrer")]
     pub temperature_tumble_stirrer: Option<Observation>,
-    #[serde(rename = "temperatureShaker")]
     pub temperature_shaker: Option<Observation>,
-    #[serde(rename = "pressureMeasurement")]
     pub pressure_measurement: Option<Observation>,
 }
 
@@ -64,18 +50,18 @@ pub enum ActionName {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ContainerInfo {
     #[serde(rename = "containerID")]
     pub container_id: String,
-    #[serde(rename = "containerBarcode")]
     pub container_barcode: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Observation {
     pub value: f64,
     pub unit: String,
-    #[serde(rename = "errorMargin")]
     pub error_margin: Option<ErrorMargin>,
 }
 
@@ -86,55 +72,46 @@ pub struct ErrorMargin {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Sample {
     #[serde(flatten)]
     pub container: ContainerInfo,
     #[serde(rename = "vialID")]
     pub vial_id: String,
-    #[serde(rename = "vialType")]
     pub vial_type: String,
     pub role: String,
-    #[serde(rename = "expectedDatum")]
     pub expected_datum: Observation,
-    #[serde(rename = "hasSample")]
     pub has_sample: Vec<SampleItem>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct SampleItem {
     #[serde(rename = "sampleID")]
     pub sample_id: String,
     pub role: String,
-    #[serde(rename = "internalBarCode")]
     pub internal_bar_code: String,
-    #[serde(rename = "expectedDatum")]
     pub expected_datum: Option<Observation>,
-    #[serde(rename = "measuredQuantity")]
     pub measured_quantity: Option<Observation>,
     pub concentration: Option<Observation>,
-    #[serde(rename = "physicalState")]
     pub physical_state: String,
-    #[serde(rename = "hasChemical")]
     pub has_chemical: Chemical,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Chemical {
     #[serde(rename = "chemicalID")]
     pub chemical_id: String,
-    #[serde(rename = "chemicalName")]
     pub chemical_name: String,
     #[serde(rename = "CASNumber")]
     pub cas_number: Option<String>,
-    #[serde(rename = "molecularMass")]
     pub molecular_mass: Observation,
     pub smiles: String,
-    #[serde(rename = "swissCatNumber")]
     pub swiss_cat_number: Option<String>,
     #[serde(rename = "Inchi")]
     pub inchi: String,
     pub keywords: Option<String>,
-    #[serde(rename = "molecularFormula")]
     pub molecular_formula: String,
     pub density: Option<Observation>,
 }
