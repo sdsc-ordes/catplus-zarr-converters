ac793b84b43b9f2c68cd9017bf17b7ccf504d5fa
cmdoret <cyril.mattheydoret@gmail.com>
2025-01-27 17:48:37 +0100

    feat(model): complete ToGraph implementation

--- synth-converter/src/models.rs
+++ synth-converter/src/models.rs
@@ -1,19 +1,14 @@
-use std::intrinsics::mir::mir;
-use std::fmt;
+use std::{fmt, intrinsics::mir::mir};
 
 // The structure follows the input data as descibed in the
 // https://github.com/sdsc-ordes/cat-plus-ontology see here for the expected Synth input data:
 // https://github.com/sdsc-ordes/cat-plus-ontology/tree/96091fd2e75e03de8a4c4d66ad502b2db27998bd/json-file/1-Synth
-use anyhow;
-use serde::{Deserialize, Serialize};
 use crate::graph::{
-    namespaces::{alloqual, allores, alloproc, cat, obo, purl, qudt, schema},
+    namespaces::{alloproc, alloqual, allores, cat, obo, purl, qudt, schema},
     utils::generate_bnode_term,
 };
-use sophia_api::{
-    ns::NsTerm,
-    term::{FromTerm, SimpleTerm, Term},
-};
+use anyhow;
+use serde::{Deserialize, Serialize};
 use sophia::{
     api::{
         graph::MutableGraph,
@@ -21,9 +16,19 @@ use sophia::{
     },
     inmem::graph::LightGraph,
 };
+use sophia_api::{
+    ns::NsTerm,
+    term::{FromTerm, SimpleTerm, Term},
+};
 
-pub fn link_node<N>(source_uri: SimpleTerm, predicate: SimpleTerm, node: N) -> Vec<[SimpleTerm; 3]> 
-    where N: ToGraph {
+fn to_graph_box<T: ToGraph + 'static>(item: T) -> Box<dyn ToGraph> {
+    Box::new(item)
+}
+
+pub fn link_node<N>(source_uri: SimpleTerm, predicate: SimpleTerm, node: N) -> Vec<[SimpleTerm; 3]>
+where
+    N: ToGraph,
+{
     let node_uri = node.get_uri();
     let mut triples = vec![[source_uri.clone(), predicate.clone(), node_uri.clone()]];
     triples.append(&mut node.to_triples(node_uri.clone()));
@@ -41,7 +46,9 @@ pub trait ToGraph {
     /// # Returns
     /// A collection of triples.
     fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
-    where 'c: 'b, 'a: 'b;
+    where
+        'c: 'b,
+        'a: 'b;
 
     /// Convert the struct to a graph.
     ///
@@ -56,7 +63,7 @@ pub trait ToGraph {
         for triple in triples {
             graph.insert(&triple[0], &triple[1], &triple[2])?;
         }
-        return Ok(graph)
+        return Ok(graph);
     }
 
     /// Get the URI for the struct.
@@ -65,11 +72,10 @@ pub trait ToGraph {
     ///
     /// # Returns
     /// The URI for the struct.
-    fn get_uri(&self) -> SimpleTerm<'static> { 
-      generate_bnode_term()
+    fn get_uri(&self) -> SimpleTerm<'static> {
+        generate_bnode_term()
     }
 }
-
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -89,15 +95,13 @@ pub struct Batch {
 
 impl ToGraph for Batch {
     fn to_triples(&self, subject: SimpleTerm) -> Vec<[SimpleTerm; 3]> {
-        let mut triples: Vec<[SimpleTerm; 3]> = [
-            (&rdf::type_, &cat::Batch),
-            (&schema::name, &self::batch_id),
-        ]
-            .into_iter().map(|(predicate, object)| {
-                [subject.clone(), predicate.as_simple(), object.as_simple()]
-        
-            })
-            .collect();
+        let mut triples: Vec<[SimpleTerm; 3]> =
+            [(&rdf::type_, &cat::Batch), (&schema::name, &self::batch_id)]
+                .into_iter()
+                .map(|(predicate, object)| {
+                    [subject.clone(), predicate.as_simple(), object.as_simple()]
+                })
+                .collect();
 
         for action in &self.actions {
             let action_subject = action.get_uri();
@@ -133,16 +137,19 @@ pub struct Action {
 }
 
 impl ToGraph for Action {
-    fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]> 
-    where 'c: 'b, 'a: 'b {
+    fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
+    where
+        'c: 'b,
+        'a: 'b,
+    {
         let subject: SimpleTerm = generate_bnode_term();
 
         // Data properties.
-        let data_properties = vec![
-            (&allores::AFX_0000622,  &self.start_time),
-            (&allores::AFR_0002423,  &self.ending_time),
-            (&allores::AFR_0001606,  &self.method_name),
-            (&allores::AFR_0001723,  &self.equipment_name),
+        let mut data_properties = vec![
+            (&allores::AFX_0000622, &self.start_time),
+            (&allores::AFR_0002423, &self.ending_time),
+            (&allores::AFR_0001606, &self.method_name),
+            (&allores::AFR_0001723, &self.equipment_name),
             (&cat::subEquipmentName, &self.sub_equipment_name),
             (&rdf::type_, &self.action_name.to_string()),
         ];
@@ -153,30 +160,41 @@ impl ToGraph for Action {
         }
 
         // Object properties
-        let object_properties: Vec<(NsTerm, Option<&dyn ToGraph>)> = vec![
-            (cat::temperatureShakerShape, self.temperature_shaker),
-            (cat::temperatureTumbleStirrerShape, self.temperature_tumble_stirrer),
-            (cat::speedInRPM, self.speed_shaker),
-            (cat::speedTumbleStirrerShape, self.speed_tumble_stirrer),
-            (alloproc::AFP_0002677, self.pressure_measurement),
-            (cat::hasSample, self.has_sample),
+        let mut object_properties: Vec<(NsTerm, Option<Box<dyn ToGraph>>)> = vec![
+            (
+                cat::temperatureShakerShape,
+                self.temperature_shaker.map(to_graph_box),
+            ),
+            (
+                cat::temperatureTumbleStirrerShape,
+                self.temperature_tumble_stirrer.map(to_graph_box),
+            ),
+            (cat::speedInRPM, self.speed_shaker.map(to_graph_box)),
+            (
+                cat::speedTumbleStirrerShape,
+                self.speed_tumble_stirrer.map(to_graph_box),
+            ),
+            (
+                alloproc::AFP_0002677,
+                self.pressure_measurement.map(to_graph_box),
+            ),
+            (cat::hasSample, self.has_sample.map(to_graph_box)),
         ];
 
-        // Multivalued data properties.
+        // Multivalued object properties.
         if let Some(container_pos) = &self.has_container_position_and_quantity {
             for container_item in container_pos {
-                object_properties.push(
-                    (cat::hasContainerPositionAndQuantity, Some(container_item)),
-                )
+                object_properties.push((
+                    cat::hasContainerPositionAndQuantity,
+                    Some(to_graph_box(*container_item)),
+                ))
             }
         }
 
         // Generate triples.
         let mut triples: Vec<[SimpleTerm; 3]> = data_properties
-            .into_iter().map(|(predicate, object)| {
-                [subject.clone(), predicate.as_simple(), object.as_simple()]
-        
-            })
+            .into_iter()
+            .map(|(predicate, object)| [subject.clone(), predicate.as_simple(), object.as_simple()])
             .collect();
 
         for (pred, object) in object_properties {
@@ -184,19 +202,18 @@ impl ToGraph for Action {
                 triples.append(&mut link_node(
                     subject.clone(),
                     pred.as_simple(),
-                    obj,
+                    obj.into(),
                 ));
             }
         }
 
         // NOTE: for container_info, we attach triples directly to the action
         if let Some(container_info) = &self.container_info {
-            triples.append(container_info.to_triples(subject.clone()));
+            triples.append(&mut container_info.to_triples(subject.clone()));
         };
 
         triples
     }
-
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -211,7 +228,6 @@ pub enum ActionName {
 }
 
 impl fmt::Display for ActionName {
-    
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let term = match self {
             Self::AddAction => cat::AddAction,
@@ -234,6 +250,26 @@ pub struct ContainerInfo {
     pub container_barcode: String,
 }
 
+impl ToGraph for ContainerInfo {
+    fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
+    where
+        'c: 'b,
+        'a: 'b,
+    {
+        let data_properties = [
+            (&cat::containerID, &self.container_id.as_simple()),
+            (&cat::containerBarcode, &self.container_barcode.as_simple()),
+        ];
+
+        let triples = data_properties
+            .into_iter()
+            .map(|(predicate, object)| [subject.clone(), predicate.as_simple(), object.to_owned()])
+            .collect();
+
+        triples
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Observation {
@@ -243,35 +279,27 @@ pub struct Observation {
 }
 
 impl ToGraph for Observation {
-    fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]> 
-    where 'c: 'b, 'a: 'b {
-
+    fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
+    where
+        'c: 'b,
+        'a: 'b,
+    {
         let data_properties = [
             (&rdf::type_, &cat::Observation.as_simple()),
             (&qudt::unit, &self.unit.as_simple()),
             (&qudt::value, &self.value.as_simple()),
         ];
 
-
-        let object_properties = [
-            (cat::errorMargin, &self.error_margin),
-        ];
+        let object_properties = [(cat::errorMargin, &self.error_margin)];
 
         let mut triples: Vec<[SimpleTerm; 3]> = data_properties
             .into_iter()
-            .map(|(predicate, object)| {
-            [subject.clone(), predicate.as_simple(), object.as_simple()]
-            })
+            .map(|(predicate, object)| [subject.clone(), predicate.as_simple(), object.as_simple()])
             .collect();
-
 
         for (pred, object) in object_properties {
             if let Some(obj) = object {
-                triples.append(&mut link_node(
-                    subject.clone(),
-                    pred.as_simple(),
-                    obj,
-                ));
+                triples.append(&mut link_node(subject.clone(), pred.as_simple(), *obj));
             }
         }
 
@@ -287,8 +315,10 @@ pub struct ErrorMargin {
 
 impl ToGraph for ErrorMargin {
     fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
-    where 'c: 'b, 'a: 'b {
-
+    where
+        'c: 'b,
+        'a: 'b,
+    {
         let data_properties = [
             (&qudt::unit, self.unit.as_simple()),
             (&qudt::value, self.value.as_simple()),
@@ -296,11 +326,8 @@ impl ToGraph for ErrorMargin {
 
         let triples = data_properties
             .into_iter()
-            .map(|(predicate, object)| {
-            [subject.clone(), predicate.as_simple(), object]
-            })
+            .map(|(predicate, object)| [subject.clone(), predicate.as_simple(), object])
             .collect();
-
 
         triples
     }
@@ -321,20 +348,41 @@ pub struct Sample {
 
 impl ToGraph for Sample {
     fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
-    where 'c: 'b, 'a: 'b {
-
+    where
+        'c: 'b,
+        'a: 'b,
+    {
         let data_properties = [
-            (&qudt::unit, self.unit.as_simple()),
-            (&qudt::value, self.value.as_simple()),
+            (&rdf::type_, cat::Sample.as_simple()),
+            (&cat::role, self.role.as_simple()),
+            (&cat::vialShape, self.vial_type.as_simple()),
+            (&allores::AFR_0002464, self.vial_type.as_simple()),
         ];
 
-        let triples = data_properties
+        let mut object_properties: Vec<(NsTerm, Box<dyn ToGraph>)> =
+            vec![(cat::expectedDatum, to_graph_box(self.expected_datum))];
+
+        // Multivalued object properties.
+        for item in &self.has_sample {
+            object_properties.push((cat::hasSample, to_graph_box(*item)))
+        }
+
+        // Generate triples
+        let mut triples: Vec<[SimpleTerm; 3]> = data_properties
             .into_iter()
-            .map(|(predicate, object)| {
-            [subject.clone(), predicate.as_simple(), object]
-            })
+            .map(|(predicate, object)| [subject.clone(), predicate.as_simple(), object])
             .collect();
 
+        for (pred, obj) in object_properties {
+            triples.append(&mut link_node(
+                subject.clone(),
+                pred.as_simple(),
+                obj.into(),
+            ));
+        }
+
+        // NOTE: for container_info, we attach triples directly to the Sample
+        triples.append(&mut self.container.to_triples(subject.clone()));
 
         triples
     }
@@ -352,6 +400,54 @@ pub struct SampleItem {
     pub concentration: Option<Observation>,
     pub physical_state: String,
     pub has_chemical: Chemical,
+}
+
+impl ToGraph for SampleItem {
+    fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
+    where
+        'c: 'b,
+        'a: 'b,
+    {
+        let data_properties: Vec<(NsTerm, Option<SimpleTerm>)> = vec![
+            (rdf::type_, cat::Sample.as_simple()),
+            (purl::identifier, self.sample_id.as_simple()),
+            (cat::role, self.role.as_simple()),
+            (cat::internalBarCode, self.internal_bar_code.as_simple()),
+            (alloqual::AFQ_0000111, self.physical_state.as_simple()),
+        ]
+        .into_iter()
+        .map(|(p, o)| (p, Some(o)))
+        .collect();
+
+        let object_properties: Vec<(NsTerm, Option<Box<dyn ToGraph>>)> = vec![
+            (cat::expectedDatum, self.expected_datum.map(to_graph_box)),
+            (
+                cat::measuredQuantity,
+                self.measured_quantity.map(to_graph_box),
+            ),
+            (allores::AFR_0002036, self.concentration.map(to_graph_box)),
+            (cat::hasChemical, Some(to_graph_box(self.has_chemical))),
+        ];
+
+        let mut triples: Vec<[SimpleTerm<'b>; 3]> = data_properties
+            .into_iter()
+            .filter(|(_, o)| o.is_some())
+            .map(|(p, o)| (p, o.unwrap()))
+            .map(|(predicate, object)| [subject.clone(), predicate.as_simple(), object])
+            .collect();
+
+        for (pred, object) in object_properties {
+            if let Some(obj) = object {
+                triples.append(&mut link_node(
+                    subject.clone(),
+                    pred.as_simple(),
+                    obj.into(),
+                ));
+            }
+        }
+
+        triples
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -372,10 +468,94 @@ pub struct Chemical {
     pub density: Option<Observation>,
 }
 
+impl ToGraph for Chemical {
+    fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
+    where
+        'c: 'b,
+        'a: 'b,
+    {
+        let mut data_properties: Vec<(NsTerm, Option<SimpleTerm>)> = vec![
+            (rdf::type_, obo::CHEBI_25367.as_simple()),
+            (purl::identifier, self.chemical_id.as_simple()),
+            (cat::chemicalName, self.chemical_name.as_simple()),
+            (allores::AFR_0001952, self.molecular_formula.as_simple()),
+            (allores::AFR_0002295, self.smiles.as_simple()),
+            (allores::AFR_0002294, self.molecular_mass.value.as_simple()),
+            (allores::AFR_0002296, self.inchi.as_simple()),
+        ]
+        .into_iter()
+        .map(|(p, o)| (p, Some(o)))
+        .collect();
+
+        // Optional data properties.
+        data_properties.append(&mut vec![
+            (cat::casNumber, self.cas_number.map(|o| o.as_simple())),
+            (
+                cat::swissCatNumber,
+                self.swiss_cat_number.map(|o| o.as_simple()),
+            ),
+            (schema::keywords, self.keywords.map(|o| o.as_simple())),
+        ]);
+
+        let object_properties: Vec<(NsTerm, Option<Box<dyn ToGraph>>)> =
+            vec![(obo::PATO_0001019, self.density.map(to_graph_box))];
+
+        // Generate triples
+        let mut triples: Vec<[SimpleTerm<'b>; 3]> = data_properties
+            .into_iter()
+            .map(|(p, o)| [subject.clone(), p.as_simple(), o.unwrap()])
+            .collect();
+
+        for (pred, object) in object_properties {
+            if let Some(obj) = object {
+                triples.append(&mut link_node(
+                    subject.clone(),
+                    pred.as_simple(),
+                    obj.into(),
+                ));
+            }
+        }
+
+        triples
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ContainerPositionQuantityItem {
     #[serde(rename = "containerID")]
     pub container_id: String,
     pub position: String,
     pub quantity: Observation,
+}
+
+impl ToGraph for ContainerPositionQuantityItem {
+    fn to_triples<'a, 'b, 'c>(&'c self, subject: SimpleTerm<'a>) -> Vec<[SimpleTerm<'b>; 3]>
+    where
+        'c: 'b,
+        'a: 'b,
+    {
+        let mut data_properties = vec![
+            (&rdf::type_, cat::ContainerPositionAndQuantity.as_simple()),
+            (&cat::containerID, self.container_id.as_simple()),
+            (&allores::AFR_0002240, self.position.as_simple()),
+        ];
+
+        let mut object_project = vec![(qudt::quantity, to_graph_box(self.quantity))];
+
+        // Generate triples
+        let mut triples: Vec<[SimpleTerm<'b>; 3]> = data_properties
+            .into_iter()
+            .map(|(predicate, object)| [subject.clone(), predicate.as_simple(), object])
+            .collect();
+
+        for (pred, object) in object_project {
+            triples.append(&mut link_node(
+                subject.clone(),
+                pred.as_simple(),
+                object.into(),
+            ));
+        }
+
+        triples
+    }
 }
