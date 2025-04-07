2ae0dfed4698e9c478b18ec596675dcb5b37c71c
sabinem <5292683+sabinem@users.noreply.github.com>
2025-04-07 12:58:31 +0200

    feat(catplus-common): add utility to generate uri term

--- src/catplus-common/src/graph/utils.rs
+++ src/catplus-common/src/graph/utils.rs
@@ -4,9 +4,19 @@ use sophia_api::{
 };
 use uuid::Uuid;
 
+pub const EX_BASE: &str = "http://example.org/";
+
 pub fn generate_bnode_term() -> SimpleTerm<'static> {
     let identifier = Uuid::new_v4().to_string();
     let bnode = BnodeId::new_unchecked(identifier);
 
     bnode.try_into_term().expect("Failed to convert BnodeId to SimpleTerm")
+}
+
+pub fn generate_iri_term() -> SimpleTerm<'static> {
+    let identifier = Uuid::new_v4().to_string();
+    let iri_string = format!("{}{}", EX_BASE, identifier);
+    let iri = IriRef::new_unchecked(iri_string);
+
+    iri.try_into_term().expect("Failed to convert Iri to SimpleTerm")
 }
