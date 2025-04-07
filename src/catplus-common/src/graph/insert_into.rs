e9be261c8aa50ebb707f61e2ad91f0e80259ff25
sabinem <5292683+sabinem@users.noreply.github.com>
2025-04-07 13:01:15 +0200

    refactor(catplus-common): have builder instead of graph as argument
    that way the building strategy is available on each insert without passing an extra argument for it

--- src/catplus-common/src/graph/insert_into.rs
+++ src/catplus-common/src/graph/insert_into.rs
@@ -1,10 +1,11 @@
-use sophia::inmem::graph::LightGraph;
+use crate::graph::{
+    graph_builder::{GraphBuilder, OutputNodeStrategy},
+    utils::{generate_bnode_term, generate_iri_term},
+};
 use sophia_api::{
     graph::MutableGraph,
     term::{SimpleTerm, Term},
 };
-
-use crate::graph::utils::generate_bnode_term;
 
 /// Used in [InsertIntoGraph::attach_and_insert].
 #[derive(Clone)]
@@ -18,18 +19,32 @@ pub struct Link<'a, 'b, 'c> {
 /// by different types.
 pub trait InsertIntoGraph {
     /// Inserts `&self` into `graph` with subject IRI `iri`
-    fn insert_into(&self, graph: &mut LightGraph, iri: SimpleTerm) -> anyhow::Result<()>;
+    fn insert_into(&self, builder: &mut GraphBuilder, iri: SimpleTerm) -> anyhow::Result<()>;
 
     /// Inserts `&self` into `graph` with subject IRI `iri` (default to a blank node)
     /// and "attach" self to an existing node with an additional triple.
-    fn attach_into(&self, graph: &mut LightGraph, attach: Link) -> anyhow::Result<()> {
-        let iri = attach.target_iri.unwrap_or_else(|| self.get_uri());
-        _ = graph.insert(&attach.source_iri, &attach.pred, &iri);
+    fn attach_into(&self, builder: &mut GraphBuilder, attach: Link) -> anyhow::Result<()> {
+        let strategy = &builder.node_strategy;
+        let target_node = match attach.target_iri {
+            Some(target) => target, // Use provided target IRI directly
+            None => {
+                // No target IRI provided, decide default based on strategy
+                match strategy {
+                    OutputNodeStrategy::Iri => self.get_uri(), // Default to IRI generation/retrieval
+                    OutputNodeStrategy::BNode => generate_bnode_term(), // Default to BNode generation
+                }
+            }
+        };
+        _ = builder.graph.insert(&attach.source_iri, &attach.pred, &target_node);
 
-        self.insert_into(graph, iri)
+        self.insert_into(builder, target_node)
     }
 
     fn get_uri(&self) -> SimpleTerm<'static> {
+        generate_iri_term()
+    }
+
+    fn get_bnode(&self) -> SimpleTerm<'static> {
         generate_bnode_term()
     }
 }
@@ -39,16 +54,16 @@ impl<T> InsertIntoGraph for Option<T>
 where
     T: InsertIntoGraph,
 {
-    fn insert_into(&self, archive: &mut LightGraph, iri: SimpleTerm) -> anyhow::Result<()> {
+    fn insert_into(&self, archive: &mut GraphBuilder, iri: SimpleTerm) -> anyhow::Result<()> {
         if let Some(v) = self {
             v.insert_into(archive, iri)?
         }
         Ok(())
     }
 
-    fn attach_into(&self, graph: &mut LightGraph, attach: Link) -> anyhow::Result<()> {
+    fn attach_into(&self, builder: &mut GraphBuilder, attach: Link) -> anyhow::Result<()> {
         if let Some(v) = self {
-            v.attach_into(graph, attach)?
+            v.attach_into(builder, attach)?
         }
         Ok(())
     }
@@ -58,16 +73,16 @@ impl<T> InsertIntoGraph for Vec<T>
 where
     T: InsertIntoGraph,
 {
-    fn insert_into(&self, graph: &mut LightGraph, _iri: SimpleTerm) -> anyhow::Result<()> {
+    fn insert_into(&self, builder: &mut GraphBuilder, iri: SimpleTerm) -> anyhow::Result<()> {
         for item in self {
-            item.insert_into(graph, item.get_uri())?;
+            item.insert_into(builder, iri.clone())?;
         }
         Ok(())
     }
 
-    fn attach_into(&self, graph: &mut LightGraph, attach: Link) -> anyhow::Result<()> {
+    fn attach_into(&self, builder: &mut GraphBuilder, attach: Link) -> anyhow::Result<()> {
         for item in self {
-            item.attach_into(graph, attach.clone())?;
+            item.attach_into(builder, attach.clone())?;
         }
         Ok(())
     }
@@ -75,14 +90,14 @@ where
 
 /// Default implementation for [SimpleTerm].
 impl<'a> InsertIntoGraph for SimpleTerm<'a> {
-    fn insert_into(&self, _graph: &mut LightGraph, _iri: SimpleTerm) -> anyhow::Result<()> {
+    fn insert_into(&self, _builder: &mut GraphBuilder, _iri: SimpleTerm) -> anyhow::Result<()> {
         unimplemented!("cannot insert {:?} into graph, use `attach_and_insert`", &self)
     }
 
-    fn attach_into(&self, graph: &mut LightGraph, attach: Link) -> anyhow::Result<()> {
+    fn attach_into(&self, builder: &mut GraphBuilder, attach: Link) -> anyhow::Result<()> {
         assert!(!self.is_triple());
 
-        _ = graph.insert(&attach.source_iri, &attach.pred, self);
+        _ = builder.graph.insert(&attach.source_iri, &attach.pred, self);
 
         Ok(())
     }
