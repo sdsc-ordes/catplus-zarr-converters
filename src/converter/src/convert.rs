277b77bb668847e5fd7f7a154d420eb72aad6cf2
cmdoret <cyril.mattheydoret@gmail.com>
2025-04-16 14:36:50 +0200

    fix(convert): cat as default prefix for materialized blank nodes

--- src/converter/src/convert.rs
+++ src/converter/src/convert.rs
@@ -30,7 +30,7 @@ where
 
     if materialize {
         graph_builder
-            .materialize_blank_nodes(Some("http://example.org/"))
+            .materialize_blank_nodes(Some("http://example.org/cat"))
             .context("Failed to materialize blank nodes")?;
     }
 
